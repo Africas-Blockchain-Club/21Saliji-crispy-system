932c2364d3dcc9d74e8054381617ff47a20fe340
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2026-09-11 10:50:56 +0200

    second draft

--- School_system/School.sol
+++ School_system/School.sol
@@ -25,26 +25,39 @@ contract ABC is AdminControl{
         string name;
         uint256 age;
         string academicLevel;
+        bool exists;
     }
 
-    mapping(uint256 => Student) public students;
+    mapping(string => mapping(uint256 => Student)) public students;
 
-    function addStudent(uint256 _id, string calldata _name, uint256 _age, string calldata _academicLevel) public onlyAdmin {
+    mapping(string => uint256[]) private studentIdsBySchool;
+
+    event StudentAdded(string indexed originSchool, uint256 id, string name);
+
+    function addStudent(
+        string calldata _originSchool,
+        uint256 _id,
+        string calldata _name,
+        uint256 _age,
+        string calldata _academicLevel
+    ) public onlyAdmin {
         require(bytes(_name).length > 0, "Name cannot be empty");
         require(_age > 0, "Age must be greater than zero");
         require(bytes(_academicLevel).length > 0, "Academic level cannot be empty");
 
-        students[_id] = Student({
+        students[_originSchool][_id] = Student({
             name: _name,
             age: _age,
-            academicLevel: _academicLevel
+            academicLevel: _academicLevel,
+            exists: true
         });
+
+        studentIdsBySchool[_originSchool].push(_id);
+        emit StudentAdded(_originSchool, _id, _name);
+    }
     }
 
-    function getStudent(uint256 _id) public view returns (Student memory) {
-        return students[_id];
+    function getStudent(string calldata _originSchool, uint256 _id) public view returns (Student memory) {
+        return students[_originSchool][_id];
     }
 
-    
-
-}
